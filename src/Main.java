fccef457b75df34325756cea24c90f42574542c9
nicholash711 <47697206+nicholash711@users.noreply.github.com>
2019-02-16 15:19:27 -0600

    window initialization

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    public Main()
+    {
+        JFrame frame = new JFrame();
+        frame.setMinimumSize(new Dimension(640,480));
+        frame.setMaximumSize(new Dimension(640,480));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args)
+    {
+        new Main();
+    }
 }
